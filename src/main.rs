c9935d1fb533e61f6d5d51b78c3c9ea821d2be0a
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-07-16 00:49:55 +0000

    able to reconcile

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
 use server::Server;
+use std::net::SocketAddr;
+use std::str::FromStr;
 use tokio::fs::{self};
 use tokio::io::copy_bidirectional;
 use tokio::net::{TcpListener, TcpStream};
-use std::net::SocketAddr;
-use std::str::FromStr;
+use tracing::error;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
-use futures::{future, FutureExt, StreamExt};
+use futures::{future, FutureExt, StreamExt, TryFutureExt};
 use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
@@ -17,12 +20,19 @@ mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "deployd=info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let listen_addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
     let server = config_server().await.expect("couldn't read server file");
-    let active_server: Arc<Mutex<Server>> = Arc::new(Mutex::new(server));
+    let active_server: Arc<Mutex<SocketAddr>> = Arc::new(Mutex::new(server));
     let config_stream =
         IntervalStream::new(time::interval(Duration::from_secs(5))).for_each(|_| async {
             let server = config_server().await.expect("couldn't read server file");
@@ -35,7 +45,7 @@ async fn main() -> Result<()> {
     let handler_stream = listener_stream.for_each_concurrent(None, |res| async {
         let addr = {
             let active = active_server.clone();
-            let x = active.lock().await.addr.clone();
+            let x = active.lock().await.clone();
             x
         };
 
@@ -46,7 +56,7 @@ async fn main() -> Result<()> {
         copy_bidirectional(&mut inbound, &mut outbound)
             .map(|r| {
                 if let Err(e) = r {
-                    println!("Failed to transfer; error={}", e);
+                    error!(error = %e, "Failed to transfer");
                 }
             })
             .await
@@ -56,12 +66,9 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn config_server() -> Result<Server> {
+async fn config_server() -> Result<SocketAddr> {
     let read = fs::read_to_string("./server").await?;
     let server_addr = read.trim();
     let server_addr = SocketAddr::from_str(server_addr)?;
-    Ok(Server {
-        name: "server".into(),
-        addr: server_addr,
-    })
+    Ok(server_addr)
 }
--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,10 @@
 use bollard::container::Config;
 use bollard::container::CreateContainerOptions;
 use bollard::container::ListContainersOptions;
-use bollard::container::NetworkingConfig;
 use bollard::container::RemoveContainerOptions;
 use bollard::container::StartContainerOptions;
 use bollard::container::StopContainerOptions;
 use bollard::secret::ContainerSummary;
-use bollard::secret::EndpointSettings;
 use bollard::secret::HostConfig;
 use bollard::secret::Mount;
 use bollard::secret::MountTypeEnum;
@@ -19,14 +17,17 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::Path;
 use thiserror::Error;
 use tokio::{fs, io};
+use tracing::debug;
 
 use std::collections::BTreeSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug, Clone)]
 pub struct Server {
     pub name: String,
     pub addr: SocketAddr,
+    service: Service,
 }
 
 #[derive(Debug, Clone, Default)]
@@ -72,15 +73,13 @@ impl Service {
         Ok(ret)
     }
 
-    pub fn container_config(&self, ip: Ipv4Addr) -> ServerResult<Config<String>> {
+    pub fn container_config(&self, ip: IpAddr) -> ServerResult<Config<String>> {
         let empty_object = HashMap::new();
-        // "ExposedPorts": {
-        //     "80/tcp": {}
-        // },
+
         let port_name = format!("{0}/tcp", self.port);
         let mut ports = HashMap::new();
         ports.insert(port_name.clone(), empty_object);
-        
+
         let ip_string = ip.to_string();
         let port_string = self.port.to_string();
         let port_binding = PortBinding {
@@ -91,14 +90,6 @@ impl Service {
         let binding_name = port_name.clone();
         port_bindings.insert(binding_name, Some(vec![port_binding]));
 
-        // "PortBindings": {
-        //     "80/tcp": [
-        //         {
-        //             "HostIp": "127.0.0.2",
-        //             "HostPort": "80"
-        //         }
-        //     ]
-        // },
         let restart_always = RestartPolicy {
             name: Some(RestartPolicyNameEnum::ALWAYS),
             maximum_retry_count: None,
@@ -139,7 +130,7 @@ impl Service {
 
 pub async fn read_env_file(path: impl AsRef<Path>) -> ServerResult<Vec<String>> {
     let read = fs::read_to_string(path).await?;
-    let lines = read.split("\n").map(|l| l.to_owned()).collect::<Vec<_>>();
+    let lines = read.split('\n').map(|l| l.to_owned()).collect::<Vec<_>>();
 
     Ok(lines)
 }
@@ -168,8 +159,19 @@ impl IpProvisioner {
 }
 
 impl Default for IpProvisioner {
-    fn default() -> Self { 
+    fn default() -> Self {
         Self::new()
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct Identifier(u64);
+
+impl Identifier {
+    fn get(&mut self) -> String {
+        let ret = self.0.to_string();
+        self.0 += 1;
+        ret
     }
 }
 
@@ -178,6 +180,7 @@ pub struct Runner {
     ips: IpProvisioner,
     containers: HashMap<String, Server>,
     docker: bollard::Docker,
+    identifier: Identifier,
 }
 
 impl Runner {
@@ -185,12 +188,65 @@ impl Runner {
         let docker = bollard::Docker::connect_with_defaults()?;
         let containers = HashMap::default();
         let ips = IpProvisioner::default();
+        let identifier = Identifier::default();
 
         Ok(Self {
             ips,
             docker,
             containers,
+            identifier,
         })
+    }
+
+    /// Maintain the containers that are running in their state
+    pub async fn execute(&mut self) {
+        // TODO: end gracefully at some point
+        loop {
+            match self.reconcile().await {
+                Ok(_) => (),
+                Err(_) => continue,
+            }
+        }
+    }
+
+    async fn reconcile(&mut self) -> ServerResult<()> {
+        // List containers we are managing
+        let mut container_names: HashSet<String> = self
+            .list_containers()
+            .await?
+            .into_iter()
+            .filter_map(|cs| {
+                cs.names?
+                    .first()
+                    .map(|n| n.trim().trim_start_matches("/").to_string())
+            })
+            .collect();
+        debug!(containers = ?container_names.iter().cloned().collect::<Vec<String>>(), "found containers");
+        // compare with desired containers
+        let mut not_found: Vec<String> = Vec::with_capacity(container_names.len());
+        for name in self.containers.keys() {
+            let found = container_names.remove(name);
+            if !found {
+                not_found.push(name.to_string())
+            }
+        }
+        // delete extras
+        for n in &container_names {
+            debug!(container = n, "stopping container");
+            self.stop(n).await?;
+        }
+        // create nonexistent
+        for nf in &not_found {
+            if self.containers.get(nf).is_none() {
+                // This shouldn't happen as we specifically iterated through containers
+                // to get the not_found
+                continue;
+            };
+            debug!(container = nf, "starting container");
+            self.run_server(nf).await?;
+        }
+
+        Ok(())
     }
 
     /// return an ip from 127.0.0.2-254
@@ -206,38 +262,58 @@ impl Runner {
         format!("deployd-{}-{}", name, identifier)
     }
 
-    pub async fn run(&mut self, service: &Service) -> ServerResult<String> {
-        let id = "1";
-        let name = Runner::container_name(service.name.clone(), id.into());
+    async fn run_server(&mut self, name: impl Into<String>) -> ServerResult<()> {
+        let name = name.into();
+        let server = self
+            .containers
+            .get(&name)
+            .ok_or(ServerError::ServerNotFound(name.clone()))?;
+
         let options = Some(CreateContainerOptions {
             name: &name,
             platform: None,
         });
-        let ip = self.reserve_ip()?;
-        let config = service.container_config(ip)?;
+        let ip = server.addr.ip();
+        let config = server.service.container_config(ip)?;
         self.docker.create_container(options, config).await?;
         self.docker
             .start_container(&name, None::<StartContainerOptions<String>>)
             .await?;
+        Ok(())
+    }
+
+    /// Add the service to be run
+    pub fn add(&mut self, service: &Service) -> ServerResult<String> {
+        let id = self.identifier.get();
+        let name = Runner::container_name(service.name.clone(), id);
+        let ip = self.reserve_ip()?;
+        let _ = service.container_config(IpAddr::V4(ip))?;
+
         let server = Server {
             name: name.clone(),
             addr: SocketAddr::new(ip.into(), service.port),
+            service: service.clone(),
         };
         self.containers.insert(name.clone(), server);
         Ok(name)
     }
 
+    /// Remove the running service from being managed
+    pub fn remove(&mut self, name: impl Into<String>) -> ServerResult<bool> {
+        let name = name.into();
+        Ok(self.containers.remove(&name).is_some())
+    }
+
     /// Stop with the given name. Return whether or not it was actually stopped
-    pub async fn stop(&mut self, name: String) -> ServerResult<bool> {
+    pub async fn stop(&mut self, name: impl Into<String>) -> ServerResult<bool> {
+        let name = name.into();
         if self.find(&name).await.is_none() {
             self.remove_container(&name);
-            return Ok(false)
+            return Ok(false);
         }
 
         // TODO: there's probably an issue if the container fails to stop or remove...
-        let options = Some(StopContainerOptions {
-            t: 30,
-        });
+        let options = Some(StopContainerOptions { t: 30 });
         self.docker.stop_container(&name, options).await?;
         let options = Some(RemoveContainerOptions {
             force: true,
@@ -254,7 +330,7 @@ impl Runner {
             if let IpAddr::V4(ip) = s.addr.ip() {
                 self.release_ip(ip);
             }
-            return true
+            return true;
         }
         false
     }
@@ -277,17 +353,35 @@ impl Runner {
 
         containers.first().cloned()
     }
+
+    async fn list_containers(&self) -> ServerResult<Vec<ContainerSummary>> {
+        let filters =
+            HashMap::from([("label".to_string(), vec!["managed-by=deployd".to_string()])]);
+        let options = Some(ListContainersOptions {
+            all: true,
+            filters,
+            ..Default::default()
+        });
+        let containers = self.docker.list_containers(options).await?;
+
+        Ok(containers)
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use test_log::test;
+
     use async_tempfile::TempFile;
     use bollard::secret::Port;
+    use tokio::time::sleep;
+    use tracing::error;
 
     use super::*;
     use std::str::FromStr;
+    use std::time::Duration;
 
-    #[tokio::test]
+    #[test(tokio::test)]
     async fn env_file_read() {
         let file = TempFile::new().await.expect("couldn't create tempfile");
         let path = file.file_path();
@@ -299,7 +393,7 @@ mod test {
         assert_eq!(envs[1], "YES=no");
     }
 
-    #[tokio::test]
+    #[test(tokio::test)]
     async fn service_with_env_file() {
         let file = TempFile::new().await.expect("couldn't create tempfile");
         let path = file.file_path();
@@ -337,22 +431,29 @@ mod test {
         };
 
         let config = svc
-            .container_config(Ipv4Addr::from_str("127.0.0.2").unwrap())
+            .container_config(IpAddr::from_str("127.0.0.2").unwrap())
             .expect("couldn't create container config");
         assert_eq!(svc.image, config.image.unwrap());
         assert_eq!(svc.env, config.env);
+
         let ports = config.exposed_ports.unwrap();
         assert_eq!(ports.len(), 1);
         assert!(ports.keys().any(|k| k.contains(&svc.port.to_string())));
 
-        let volumes = config.volumes.unwrap();
+        let host_config = config.host_config.expect("didn't get host_config");
+        assert!(host_config.port_bindings.is_some(), "no port bindings");
+
+        let volumes = host_config.mounts.expect("didn't get mounts");
         assert_eq!(volumes.len(), 1);
         let mapping = svc.volume_mapping.unwrap();
         let source = mapping.source;
         let destination = mapping.destination;
-
-        assert!(volumes.keys().any(|k| k.contains(&source)));
-        assert!(volumes.keys().any(|k| k.contains(&destination)));
+        assert!(volumes
+            .iter()
+            .any(|v| v.source.clone().unwrap().contains(&source)));
+        assert!(volumes
+            .iter()
+            .any(|v| v.target.clone().unwrap().contains(&destination)));
     }
 
     #[test]
@@ -374,7 +475,7 @@ mod test {
     }
 
     #[ignore]
-    #[tokio::test]
+    #[test(tokio::test)]
     async fn run_service() {
         let mut runner = Runner::new().expect("couldn't create runner");
         let svc = Service {
@@ -384,7 +485,9 @@ mod test {
             env: None,
             volume_mapping: None,
         };
-        let name = runner.run(&svc).await.unwrap();
+
+        let name = runner.add(&svc).expect("couldn't add server");
+        runner.run_server(&name).await.unwrap();
         // let docker = bollard::Docker::connect_with_defaults().expect("couldn't create test docker");
         let info = runner.find(&name).await.expect("error finding container");
         let image = info.image.expect("no image found");
@@ -396,18 +499,79 @@ mod test {
         assert!(container_name.contains("deployd"));
 
         let ports = info.ports.expect("no ports found");
-        let ports = ports.into_iter().filter(|p| {
-            if let Some(ip) = &p.ip {
-                return IpAddr::from_str(ip).is_ok_and(|ip| ip.is_loopback());
-            }
-            false
-        })
-        .collect::<Vec<Port>>();
-        assert!(ports.len() > 0);
+        let ports = ports
+            .into_iter()
+            .filter(|p| {
+                if let Some(ip) = &p.ip {
+                    return IpAddr::from_str(ip).is_ok_and(|ip| ip.is_loopback());
+                }
+                false
+            })
+            .collect::<Vec<Port>>();
+        assert!(!ports.is_empty());
         let port = ports.first().unwrap();
         assert_eq!(8080, port.private_port, "private port");
         assert_eq!(8080, port.public_port.unwrap(), "public port");
 
         runner.stop(name).await.unwrap();
+    }
+
+    #[ignore]
+    #[test(tokio::test)]
+    async fn test_list_containers() {
+        let mut runner = Runner::new().expect("couldn't create runner");
+        let svc = Service {
+            name: "test".to_string(),
+            port: 8080,
+            image: "nginx".to_string(),
+            env: None,
+            volume_mapping: None,
+        };
+        let name = runner.add(&svc).expect("couldn't add server");
+        runner.run_server(&name).await.unwrap();
+        let containers = runner.list_containers().await.unwrap();
+        assert_eq!(containers.len(), 1);
+
+        runner.stop(name).await.unwrap();
+        let containers = runner.list_containers().await.unwrap();
+        assert_eq!(containers.len(), 0);
+    }
+
+    #[ignore]
+    #[test(tokio::test)]
+    async fn test_reconcile() {
+        let mut runner = Runner::new().expect("couldn't create runner");
+        runner.reconcile().await.expect("couldn't reconcile");
+        let containers = runner.list_containers().await.unwrap();
+        assert_eq!(containers.len(), 0);
+
+        let svc = Service {
+            name: "test".to_string(),
+            port: 8080,
+            image: "nginx".to_string(),
+            env: None,
+            volume_mapping: None,
+        };
+        let name = runner.add(&svc).expect("couldn't add service");
+        runner.reconcile().await.expect("couldn't reconcile");
+        let containers = runner.list_containers().await.unwrap();
+        assert_eq!(containers.len(), 1);
+
+        runner.remove(&name).expect("couldn't remove server");
+        for _ in 0..40 {
+            let _ = runner
+                .reconcile()
+                .await
+                .inspect_err(|e| error!(error = %e, "couldn't reconcile"));
+            sleep(Duration::from_secs(1)).await;
+            if let Ok(containers) = runner.list_containers().await {
+                if containers.is_empty() {
+                    break;
+                }
+            }
+        }
+
+        let containers = runner.list_containers().await.unwrap();
+        assert_eq!(containers.len(), 0);
     }
 }
